ecb411a920c6b0ec9db03fbbc41b47b007352f35
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-07-09 22:35:29 +1000

    Setup the project

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./NavBar";
+import MailboxDetails from "./MailboxDetails";
+import MailboxForm from "./MailboxForm";
+import MailboxList from "./MailboxList";
+
+const App = () => {
+  return <h1>Hello world!</h1>;
+};
+
+export default App;
